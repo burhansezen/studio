ca9fc1e7b2985fbb8d83fe2b51848ebc42da8058
Burhan Sezen <burhansezen42@gmail.com>
2025-10-28 21:49:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6532717915-cfb7d",
+  "appId": "1:436265983743:web:3bee6c9d35d62d63ee76ca",
+  "apiKey": "AIzaSyDOQNaQgqTnIfSPBnqmyRJfXesxNk5IquY",
+  "authDomain": "studio-6532717915-cfb7d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "436265983743"
+};
